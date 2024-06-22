1580e80e69c251e7031be9eb0486d400a7c97ce4
kshitij7704 <kashyapkshitij7704@gmail.com>
2024-06-22 13:56:36 +0530

    Completed add task operation

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///list.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'instance', 'list.db')
+
+# Ensure the 'instances' directory exists
+if not os.path.exists(os.path.join(basedir, 'instance')):
+    os.makedirs(os.path.join(basedir, 'instance'))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -16,7 +25,23 @@ class Todo(db.Model):
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        if not task_content.strip():
+            return "The task content cannot be empty."
+
+        new_task = ToDo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return f"There was an issue adding your task: {e}"
+
+    else:
+        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)

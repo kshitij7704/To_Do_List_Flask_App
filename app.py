a9a1e86f5471a06897408b76ba51cdebaedb3b1e
kshitij7704 <kashyapkshitij7704@gmail.com>
2024-06-22 16:52:50 +0530

    Added complete code

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+
+# Finding the directory for sqllte database
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'instance', 'list.db')
 
-# Ensure the 'instances' directory exists
 if not os.path.exists(os.path.join(basedir, 'instance')):
     os.makedirs(os.path.join(basedir, 'instance'))
 
+
+# Configuring the sqlite database URI and disable sqlalchemy modification tracking
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_path
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
+# Defining the ToDo model for the database
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
@@ -23,9 +28,12 @@ class ToDo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
+
+# Route for the main page
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        # For new task submission
         task_content = request.form['content']
         if not task_content.strip():
             return "The task content cannot be empty."
@@ -40,10 +48,12 @@ def index():
             return f"There was an issue adding your task: {e}"
 
     else:
+        # Retrieval and display all tasks
         tasks = ToDo.query.order_by(ToDo.date_created).all()
         return render_template("index.html", tasks=tasks)
-        
 
+
+# Route for deleting a task
 @app.route("/delete/<int:id>")
 def delete(id):
     task_to_delete = ToDo.query.get_or_404(id)
@@ -56,10 +66,12 @@ def delete(id):
         return "There was an issue deleting the task"
 
 
+# Route for updating a task
 @app.route("/update/<int:id>", methods=['POST', 'GET'])
 def update(id):
     task= ToDo.query.get_or_404(id)
     if request.method == 'POST':
+        # Update the task
         task.content = request.form['content']
 
         try:
@@ -69,8 +81,10 @@ def update(id):
             return "There was an issue updating your task"
 
     else:
+        # Display the update task page
         return render_template("update.html", task=task)
 
 
+# Main function for running the flask app
 if __name__ == "__main__":
     app.run(debug=True)
--- create_db.py
+++ create_db.py
@@ -1,5 +1,6 @@
 from app import db, app
 
 with app.app_context():
+    # Creating all databases defined in app
     db.create_all()
     print("Database tables created successfully.")
